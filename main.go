161642afa9e3d288ac5aa65f31e00db0cbe4de13
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-10-16 20:39:37 -0700

    main: set default values for config

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ package main
 #include <libavdevice/avdevice.h>
 #include "c/segmenter.h"
 #include "c/util.h"
+
 #cgo LDFLAGS: -L${SRCDIR}/build -lsegmenter -lavcodec -lavformat -lavutil
 */
 import "C"
 
 import (
 	"fmt"
+	"os"
 	"unsafe"
 
 	"github.com/3d0c/gmf"
@@ -23,15 +25,29 @@ type config struct {
 	MediaFileName   string
 	IndexFile       string
 	SourceFile      string
-	Media           int
 	Stat            int
 	PlaylistEntries int
-	Duration        int
+	Duration        float64
 }
 
 func segment(cfg config) error {
 	var sourceContext *C.struct_AVFormatContext
 	var outputContext *C.SegmenterContext
+
+	if cfg.MediaBaseName == "" {
+		cfg.MediaBaseName = "fileSequence"
+	}
+
+	if cfg.Duration == 0 {
+		cfg.Duration = 10
+	}
+
+	if cfg.IndexFile == "" {
+		cfg.IndexFile = "prog_index.m3u8"
+	}
+
+	//TODO improve the way we create the folder
+	os.Mkdir(cfg.FileBase, 0777)
 
 	C.av_register_all()
 
