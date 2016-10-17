eff1abb965969ca05d91ef95ce693e190b452917
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-10-16 20:53:33 -0700

    main: extract segmenter logics to different file

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ all:
 	$(CC) -Wno-deprecated -g -fPIC -c -o build/segmenter.o c/segmenter.c
 	$(CC) -Wno-deprecated -g -fPIC -c -o build/util.o c/util.c
 	$(CC) -g -lavcodec -lavformat -lavutil -fPIC -shared -o build/libsegmenter.so build/segmenter.o build/util.o
-	LD_LIBRARY_PATH=. go build main.go
+	LD_LIBRARY_PATH=. go build *.go
---
+++ segmenter.go
@@ -0,0 +1,93 @@
+package main
+
+/*
+#include <stdio.h>
+#include "libavformat/avformat.h"
+#include <libavdevice/avdevice.h>
+#include "c/segmenter.h"
+#include "c/util.h"
+
+#cgo LDFLAGS: -L${SRCDIR}/build -lsegmenter -lavcodec -lavformat -lavutil
+*/
+import "C"
+
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"github.com/3d0c/gmf"
+)
+
+//HLSConfig stores all the configurations needed for HLS outputs
+type HLSConfig struct {
+	BaseURL         string
+	FileBase        string
+	MediaBaseName   string
+	IndexFile       string
+	SourceFile      string
+	Stat            int
+	PlaylistEntries int
+	Duration        float64
+}
+
+// Segment function is responsible for the HLS generation
+func Segment(cfg HLSConfig) error {
+	var sourceContext *C.struct_AVFormatContext
+	var outputContext *C.SegmenterContext
+
+	cfg = setDefaultValues(cfg)
+
+	//TODO improve the way we create the folder
+	os.Mkdir(cfg.FileBase, 0777)
+
+	C.av_register_all()
+
+	sourceFile := C.CString(cfg.SourceFile)
+	defer C.free(unsafe.Pointer(sourceFile))
+
+	if averr := C.avformat_open_input(&sourceContext, sourceFile, nil, nil); averr < 0 {
+		return fmt.Errorf("Error opening input: %s", gmf.AvError(int(averr)))
+	}
+	defer C.avformat_free_context(sourceContext)
+
+	if averr := C.avformat_find_stream_info(sourceContext, nil); averr < 0 {
+		return fmt.Errorf("Error finding stream info: %s", gmf.AvError(int(averr)))
+	}
+
+	if ret := C.segmenter_alloc_context(&outputContext); ret < 0 {
+		return fmt.Errorf("Cannot allocate context: %s", C.GoString(C.sg_strerror(C.int(ret))))
+	}
+	defer C.segmenter_free_context(outputContext)
+
+	fileBase := C.CString(cfg.FileBase)
+	defer C.free(unsafe.Pointer(fileBase))
+
+	mediaBaseName := C.CString(cfg.MediaBaseName)
+	defer C.free(unsafe.Pointer(mediaBaseName))
+
+	if ret := C.segmenter_init(outputContext, sourceContext, fileBase, mediaBaseName, C.double(cfg.Duration), C.int(3)); ret != 0 {
+		return fmt.Errorf("Cannot initialize segmenter: %s", C.GoString(C.sg_strerror(C.int(ret))))
+	}
+
+	if ret := C.segmenter_open(outputContext); ret < 0 {
+		return fmt.Errorf("open output: %s", C.GoString(C.sg_strerror(C.int(ret))))
+	}
+	return nil
+}
+
+func setDefaultValues(cfg HLSConfig) HLSConfig {
+	if cfg.MediaBaseName == "" {
+		cfg.MediaBaseName = "fileSequence"
+	}
+
+	if cfg.Duration == 0 {
+		cfg.Duration = 10
+	}
+
+	if cfg.IndexFile == "" {
+		cfg.IndexFile = "prog_index.m3u8"
+	}
+
+	return cfg
+}
